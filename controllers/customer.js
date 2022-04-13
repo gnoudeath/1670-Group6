c3a3bc52a0ce64ea3dbd9afb10e292e1ea94ee04
tunagch190847 <91232125+tunagch190847@users.noreply.github.com>
2022-04-13 23:33:07 +0700

    updateBackendSearch

--- controllers/customer.js
+++ controllers/customer.js
@@ -24,13 +24,82 @@ router.use((req, res, next) => {
     }
   });
 
-  
 
 router.get('/index', async (req, res) =>{
   const book = await dbHandler.getAllProducts();
   res.render('index', { book:book });
 
 })
+
+//search
+router.get("/search", async (req, res) => {
+  const book = await dbHandler.getAllProducts();
+  const searchInput = req.query.searchInput;
+  if (isNaN(Number.parseFloat(searchInput)) == false) {
+    await SearchObject(
+      req,
+      searchInput,
+      book,
+      res,
+      dbHandler.searchObjectbyPrice,
+      "Book",
+      Number.parseFloat(searchInput),
+      " VND"
+    );
+  } else {
+    await SearchObject(
+      req,
+      searchInput,
+      book,
+      res,
+      dbHandler.searchObjectbyName,
+      "Book",
+      searchInput,
+      ""
+    );
+  }
+});
+
+async function SearchObject(
+  req,
+  searchInput,
+  book,
+  res,
+  dbFunction,
+  collectionName,
+  searchInput,
+  mess
+) {
+  const resultSearch = await dbFunction(collectionName, searchInput);
+  if (resultSearch.length != 0) {
+    if (!req.session.user) {
+      res.render("search", {
+        searchBook: resultSearch,
+        book:book,
+      });
+    } else {
+      res.render("search", {
+        searchBook: resultSearch,
+        book:book,
+      });
+    }
+  } else {
+    if (!req.session.user) {
+      const message = "Not found " + searchInput + mess;
+      res.render("search", {
+        book:book,
+        errorSearch: message,
+      });
+    } else {
+      const message = "Not found " + searchInput + mess;
+      res.render("search", {
+        book:book,
+        errorSearch: message,
+        user: req.session.user,
+      });
+    }
+  }
+}
 
 
 module.exports = router;
