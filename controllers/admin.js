a7f9751fdc665b78cd60f2685d66bb09b04ced9b
dungnagch190701 <87683549+dungnagch190701@users.noreply.github.com>
2022-04-14 11:41:52 +0700

    connect category with book

--- controllers/admin.js
+++ controllers/admin.js
@@ -42,16 +42,17 @@ router.get('/product', async (req, res) => {
 
 //addbook
 router.get('/addbook', async (req, res)=> {
-    res.render("AddBook")
+    const cat = await dbHandler.getAllCategory();
+    res.render("AddBook",{cat:cat})
 })
 router.post('/addbook', async (req, res) => {
     const nameInput = req.body.txtName
     const priceInput = req.body.txtPrice
     const image = req.body.txtImage
     const Description = req.body.txtDescription
-    const Category = req.body.Category
-    const CategoryID = await dbHandler.getAllCategory("Category" , Category)
-    const newBook = {name:nameInput, des:Description, price:Number.parseFloat(priceInput), pic:image, cat_name:CategoryID._id}
+    const cat = req.body.cat
+    const catid = await dbHandler.getDocumentById(cat,'Category')
+    const newBook = {name:nameInput, des:Description, price:Number.parseFloat(priceInput), pic:image, cat:catid}
     await dbHandler.insertObject("Book", newBook)
     res.redirect('/admin/product')
 })
