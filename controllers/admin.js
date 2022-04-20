e3a2f933bd7cb5ed0cf0d2ac7b5857a7e496d333
gnoudeath <87683653+gnoudeath@users.noreply.github.com>
2022-04-20 22:27:10 +0700

    search feedback

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,6 +9,7 @@ const bcrypt = require("bcrypt");
 const {MongoClient, Int32, Db} = require('mongodb')
 const async = require('hbs/lib/async')
 const { route } = require('./customer')
+const { query } = require('express')
 const url = "mongodb+srv://new-duong-0805:123456789td@cluster0.pbe5o.mongodb.net/test";
 const client = new MongoClient(url, {useNewUrlParser: true,useUnifiedTopology: true });
 
@@ -130,12 +131,6 @@ router.post("/updatestatus", async (req,res)=>{
     res.redirect('/admin')
 })
 
-//feedback manage
-
-router.get("/feedbackmanage", async (req, res) =>{
-    let result = await dbHandler.getAllFB("Feedback");
-    res.render('feedbackmanage', {feedback: result, user: req.session.user})
-});
 router.get('/', async (req, res) =>{
     
     // const client = await MongoClient.connect(url);   
@@ -162,50 +157,50 @@ router.get('/', async (req, res) =>{
 
 })
 
-router.get("/:findOrder", async (req, res, next) => { //sortby same tham số
-    let result = await dbHandler.getAllCustomerOrder();
-    if (req.params.findOrder === "today") {
-        let today = new Date().toLocaleDateString("vi");//lưu new Date.toLocaledatestring = today; today ở dạng stirng
-        result = result.filter((item) => { //dùng filter cho biến result lọc ra các phần từ có đk là dòng 53. sau đó gán lại vào result
-            item.itemString = "";
-            item.books.forEach(e => {//tao bien itemString de hien thi cac phan tu trong element (them item va amount)
-                item.itemString += e.name + " - (" + e.qty + ")";
-            })
-            return item.time.toLocaleDateString("vi") === today
-        });
-        result.forEach((element) => { //dùng loop cho tất cả result để hiển thị time theo dang string
-            element.time = element.time.toLocaleString("vi");
-        });
-        res.render("adminPage", { customerOrder: result }); //truyền vào tất cả các result vừa được xử lý
+// router.get("/:findOrder", async (req, res, next) => { //sortby same tham số
+//     let result = await dbHandler.getAllCustomerOrder();
+//     if (req.params.findOrder === "today") {
+//         let today = new Date().toLocaleDateString("vi");//lưu new Date.toLocaledatestring = today; today ở dạng stirng
+//         result = result.filter((item) => { //dùng filter cho biến result lọc ra các phần từ có đk là dòng 53. sau đó gán lại vào result
+//             item.itemString = "";
+//             item.books.forEach(e => {//tao bien itemString de hien thi cac phan tu trong element (them item va amount)
+//                 item.itemString += e.name + " - (" + e.qty + ")";
+//             })
+//             return item.time.toLocaleDateString("vi") === today
+//         });
+//         result.forEach((element) => { //dùng loop cho tất cả result để hiển thị time theo dang string
+//             element.time = element.time.toLocaleString("vi");
+//         });
+//         res.render("adminPage", { customerOrder: result }); //truyền vào tất cả các result vừa được xử lý
 
-    } else if (req.params.findOrder === "week") {
-        let today = new Date(); //gán today bằng ngày hn
-        let week = new Date(today.setDate(today.getDate() - 7)); //week = lấy (today.getDate -7) là dạng số. sau đó setDate lại về dạng thời gian
-        result = result.filter((item) => { //item là 1 phần tử của result
-            item.itemString = "";
-            item.books.forEach(e => {//tao bien itemString de hien thi cac phan tu trong element (them item va amount)
-                item.itemString += e.name + " - (" + e.qty + ")";
-            })
-            return item.time > week //dk ptu có điều kiện item > week
-        });
-        result.forEach((element) => {//dùng loop cho result sau đó chuyển element.time về dạng string theo keieur vn
-            element.time = element.time.toLocaleString("vi");
-        });
-        res.render("adminPage", { customerOrder: result }); //truyền vào tất cả result vừa được xử lý
-    }
-    else if (req.params.findOrder === "delete") {//tham số sortby là delete
-        let { id } = req.query; // same as: let id = req.query.id;
-        let result = await dbHandler.deleteOne("CustomerOrder", { _id: ObjectId(id) });//dùng hàm deOne để xóa 1 document trong collection customer order
-        if (result == null) { 
-           res.send("Cancel error!"); 
-        } else {
-            res.redirect("/admin");
-        }
-    }
-    else {
-        next("route");
-    }
-});
+//     } else if (req.params.findOrder === "week") {
+//         let today = new Date(); //gán today bằng ngày hn
+//         let week = new Date(today.setDate(today.getDate() - 7)); //week = lấy (today.getDate -7) là dạng số. sau đó setDate lại về dạng thời gian
+//         result = result.filter((item) => { //item là 1 phần tử của result
+//             item.itemString = "";
+//             item.books.forEach(e => {//tao bien itemString de hien thi cac phan tu trong element (them item va amount)
+//                 item.itemString += e.name + " - (" + e.qty + ")";
+//             })
+//             return item.time > week //dk ptu có điều kiện item > week
+//         });
+//         result.forEach((element) => {//dùng loop cho result sau đó chuyển element.time về dạng string theo keieur vn
+//             element.time = element.time.toLocaleString("vi");
+//         });
+//         res.render("adminPage", { customerOrder: result }); //truyền vào tất cả result vừa được xử lý
+//     }
+//     else if (req.params.findOrder === "delete") {//tham số sortby là delete
+//         let { id } = req.query; // same as: let id = req.query.id;
+//         let result = await dbHandler.deleteOne("CustomerOrder", { _id: ObjectId(id) });//dùng hàm deOne để xóa 1 document trong collection customer order
+//         if (result == null) { 
+//            res.send("Cancel error!"); 
+//         } else {
+//             res.redirect("/admin");
+//         }
+//     }
+//     else {
+//         next("route");
+//     }
+// });
 
 router.get("/admin", async (req, res) => {
     let result = await dbHandler.getAllCustomerOrder();
@@ -214,4 +209,54 @@ router.get("/admin", async (req, res) => {
 
 });
 
+//feedback manage
+
+router.get("/feedbackmanage", async (req, res) =>{
+    let result = await dbHandler.getAllFB("Feedback");
+    res.render('feedbackmanage', {feedback: result, user: req.session.user})
+});
+
+router.get("/feedbackmanage/delete", async(req, res) =>{
+    await dbHandler.deleteDocumentById('Feedback', req.query.id);
+    res.redirect('/admin/feedbackmanage');
+});
+
+router.get("/feedbackmanage/:day", async (req, res, next) =>{
+    let result = await dbHandler.getAllFB("Feedback");
+    const today = new Date();
+    if (req.params.day == "today"){
+        result = result.filter((f) =>{
+            return new Date(f.time).toDateString() === today.toDateString(); //chuyển f.time về string dạng ngày 
+        });
+        res.render("feedbackmanage", {
+            feedback: result,
+            user: req.session.user,
+        });
+    }
+    else if (req.params.day === "1weeks"){
+        const queryTimeDay = new Date(today.setDate(today.getDate() -7)); //-7 day 
+        result = result.filter((f) => new Date(f.time) > queryTimeDay);
+        res.render("feedbackmanage", {
+            feedback: result,
+            user: req.session.user,
+        });
+    }
+    else if (req.params.day === "1months"){
+        const queryTimeDay = new Date(today.setMonth(today.getMonth() -1)); //-30 day 
+        result = result.filter((f) => new Date(f.time) > queryTimeDay);
+        res.render("feedbackmanage", {
+            feedback: result,
+            user: req.session.user,
+        });
+    }
+    else{
+        next("route");
+    }
+});
+
+router.get("/feedbackmanage/searchFeedback", async(req, res) =>{
+    const searchInput = req.query.bookName;
+    const result = await dbHandler.searchObjectbyName("Feedback", searchInput)
+    res.render("feedbackmanage", {feedback: result})
+});
 module.exports = router;
